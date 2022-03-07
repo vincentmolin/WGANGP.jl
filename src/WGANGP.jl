df3ff3a56276ef80fcc4a29e8a6eeaadb22a065c
vincentmolin <vincentmolin@gmail.com>
2022-03-07 11:57:11 +0100

    rename

--- src/WGANGP.jl
+++ src/WGANGP.jl
@@ -5,7 +5,7 @@ using Zygote
 using Flux
 using Flux: mean, pullback, params
 
-include("wgangp.jl")
+include("wgan-gp.jl")
 export lipschitz1_gradient_loss, critic_loss, 
        generator_loss, step_critic!, step_generator!
 
