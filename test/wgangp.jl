75927390885136cd28074475a8df02e03d168480
vincentmolin <vincentmolin@gmail.com>
2022-08-19 13:12:08 +0200

    improve tests

--- test/wgangp.jl
+++ test/wgangp.jl
@@ -6,6 +6,11 @@ if CUDA.functional()
     Random.seed!(123)
     
     @testset "train steps 3d data" begin
+
+        image_dim = 64
+        batch_size = 8
+        latent_dim = 4
+
         crit = Chain(
             # Input: (64, 64, 1, :)
             Conv((4, 4), 1 => 2, selu, stride = 2, pad = SamePad()),   # -> (32, 32, 2, :)
@@ -15,12 +20,28 @@ if CUDA.functional()
             Dropout(0.2),
             Dense(8 * 8 * 8, 1)
         ) |> gpu
-        image_dim = 64
-        batch_size = 32
+
+        genr = Chain(
+            Dense(latent_dim, 16 * 16 * 8, relu),
+            x -> reshape(x, 16, 16, 8, :),
+            ConvTranspose((5, 5), 8 => 4; stride = 1, pad = 2),
+            BatchNorm(4, relu),
+            ConvTranspose((4, 4), 4 => 2; stride = 2, pad = 1),
+            BatchNorm(2, relu),
+            ConvTranspose((4, 4), 2 => 1, sigmoid; stride = 2, pad = 1)
+        ) |> gpu
+
         opt_crit = Adam(0.0002, (0.5, 0.9))
+        opt_genr = Adam(0.0002, (0.5, 0.9))
         x_true = cu(randn(Float32, image_dim, image_dim, 1, batch_size))
         x_generated = cu(randn(Float32, image_dim, image_dim, 1, batch_size))
         @test step_critic!(opt_crit, crit, x_true, x_generated) isa Float32
+        
+        z = cu(randn(Float32, latent_dim, batch_size))
+        @test step_generator!(opt_genr, genr, crit, z) isa Float32
+
+        @test any(x -> any(isnan,x), Flux.params(crit))
+        @test any(x -> any(isnan,x), Flux.params(genr))
     end
     
     @testset "train steps 1d data" begin
