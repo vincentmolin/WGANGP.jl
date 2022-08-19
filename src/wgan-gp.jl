3680f47691eccb29dff54d5b4f9c88b46231f4fa
vincentmolin <vincentmolin@gmail.com>
2022-08-19 13:13:40 +0200

    update losses

--- src/wgan-gp.jl
+++ src/wgan-gp.jl
@@ -1,31 +1,36 @@
-# Define gradients for rand and ones to make the lipschitz loss autodiff'able
+# Define CUDA gradients to make the lipschitz loss autodiff'able
 Zygote.@adjoint CUDA.ones(x...) = CUDA.ones(x...), _ -> map(_ -> nothing, x)
+Zygote.@adjoint CUDA.zeros(x...) = CUDA.zeros(x...), _ -> map(_ -> nothing, x)
 Zygote.@adjoint CUDA.rand(x...) = CUDA.rand(x...), _ -> map(_ -> nothing, x)
+Zygote.@adjoint CUDA.fill(x::Real, dims...) = fill(x, dims...), Δ->(sum(Δ), map(_->nothing, dims)...)
 
 """
     lipschitz1_gradient_loss(m, x_true, x_generated)
 Estimates 𝐄ₓ(‖∇ₓD(x)‖₂ - 1)², where x is sampled uniformly on lines between
 points from the data distribution and the generators distribution
 """
-function lipschitz1_gradient_loss(m, x_true, x_generated)
-    x_size = size(x_true)
-    batch_size = x_size[end]
-    data_dims = length(x_size) - 1
-    ξ_size = tuple(ones(Int,data_dims)..., batch_size)
-    ξ = CUDA.rand(ξ_size...)
-    x_interpolated = ξ .* x_true + (1.0f0 .- ξ) .* x_generated
+function lipschitz1_gradient_loss(m, x_interpolated, batch_size, data_dims)
+    # x_size = size(x_true)
+    # data_dims = length(x_size) - 1
+    # ξ_size = tuple(ones(Int,data_dims)..., batch_size)
+    # CUDA.@sync ξ = CUDA.rand(ξ_size...)
+    # x_interpolated = ξ .* x_true + (1.0f0 .- ξ) .* x_generated
     _, b = pullback(() -> m(x_interpolated), params(x_interpolated))
     grads = b(CUDA.ones(1, batch_size))
     sqddx = grads[x_interpolated] .^ 2
     mean((sqrt.(sum(sqddx, dims = 1:data_dims)) .- 1.0f0) .^ 2)
 end
 
+#function gpu_gradient_loss(m, x_interpolated)
+#    mean((sqrt.(sum(abs2, gradient(sum∘m, x_interpolated)[1], dims = (1:ndims(x_interpolated)-1))) .- 1) .^ 2)
+#end
+
 """
     critic_loss(m, x_true, x_generated, batch_size, λ)
 WGAN-GP relaxed critic loss with lagrange multiplier λ
 """
-function critic_loss(m, x_true, x_generated, λ)
-    gp = lipschitz1_gradient_loss(m, x_true, x_generated)
+function critic_loss(m, x_true, x_generated, x_interpolated, λ, batch_size, data_dims)
+    gp = lipschitz1_gradient_loss(m, x_interpolated, batch_size, data_dims)
     return mean(m(x_generated)) - mean(m(x_true)) + λ * gp
 end
 
@@ -33,14 +38,25 @@ function generator_loss(m, crit, z)
     -mean(crit(m(z)))
 end
 
+function interpolate_x(x_true, x_generated, batch_size, data_dims)
+    ξ = CUDA.rand(ones(Int64, data_dims)..., batch_size)
+    x_interpolated = ξ .* x_true + (1.0f0 .- ξ) .* x_generated
+    x_interpolated
+end
+
+Zygote.@nograd interpolate_x
+
 """
     step_critic!(opt, m, x_true, x_generated; λ = 10.0f0)
 A single optimisation step for the critic, with λ gradient penalty factor.
 """
 function step_critic!(opt, m, x_true, x_generated; λ = 10.0f0)
+    batch_size = size(x_true)[end]
+    data_dims = ndims(x_true) - 1
+    x_interpolated = interpolate_x(x_true, x_generated, batch_size, data_dims)
     ps = params(m)
     loss, back = pullback(ps) do
-        critic_loss(m, x_true, x_generated, λ)
+        critic_loss(m, x_true, x_generated, x_interpolated, λ, batch_size, data_dims)
     end
     gs = back(1.0f0)
     Flux.update!(opt, ps, gs)
@@ -59,11 +75,3 @@ function step_generator!(opt, m, crit, z)
     Flux.update!(opt, ps, gs)
     return loss
 end
-
-
-# Alternatively, this could be done outside the pullback in step_critic! 
-# function interpolate_x(x_true, x_generated, batch_size)
-#    ξ = CUDA.rand(1, 1, 1, batch_size)
-#    x_interpolated = ξ .* x_true + (1.0f0 .- ξ) .* x_generated
-# end
-# Zygote.@nograd interpolate_x
